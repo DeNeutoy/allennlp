1f2a5f5f075b3bec7cbce659d789670faab312d1
Mark Neumann <markng@allenai.org>
2018-01-15 21:19:01 +0000

    improve script for eval

--- scripts/write_srl_predictions_to_conll_format.py
+++ scripts/write_srl_predictions_to_conll_format.py
@@ -11,47 +11,55 @@ from allennlp.models import Model
 from allennlp.models.semantic_role_labeler import write_to_conll_eval_file
 
 
-def main(serialization_directory, device):
+def main(serialization_directory, device, data, prefix):
     """
     serialization_directory : str, required.
-        The directory containing the serialized weights.
+        the directory containing the serialized weights.
     device: int, default = -1
-        The device to run the evaluation on.
+        the device to run the evaluation on.
+    data: str, default = None
+        The data to evaluate on. By default, we use the validation data from
+        the original experiment.
+    prefix: str, default=""
+        The prefix to prepend to the generated gold and prediction files, to distinguish
+        different models/data.
     """
 
     config = Params.from_file(os.path.join(serialization_directory, "model_params.json"))
     dataset_reader = DatasetReader.from_params(config['dataset_reader'])
-    evaluation_data_path = config['validation_data_path']
+
+    evaluation_data_path = data if data else config['validation_data_path']
 
     model = Model.load(config, serialization_dir=serialization_directory, cuda_device=device)
+    model.eval()
 
-    prediction_file_path = os.path.join(serialization_directory, "predictions.txt")
-    gold_file_path = os.path.join(serialization_directory, "gold.txt")
+    prediction_file_path = os.path.join(serialization_directory, prefix + "_predictions.txt")
+    gold_file_path = os.path.join(serialization_directory, prefix + "_gold.txt")
     prediction_file = open(prediction_file_path, "w+")
     gold_file = open(gold_file_path, "w+")
 
-    # Load the evaluation data and index it.
-    print("Reading evaluation data from {}".format(evaluation_data_path))
+    # load the evaluation data and index it.
+    print("reading evaluation data from {}".format(evaluation_data_path))
     dataset = dataset_reader.read(evaluation_data_path)
     dataset.index_instances(model.vocab)
     iterator = BasicIterator(batch_size=32)
 
     model_predictions = []
-    for batch in tqdm.tqdm(iterator(dataset, num_epochs=1, shuffle=False, cuda_device=device, for_training=False)):
-        result = model(**batch)
+    for batch in tqdm.tqdm(iterator(dataset, num_epochs=1, shuffle=False, for_training=True, cuda_device=device)):
+        result = model.forward(**batch)
         predictions = model.decode(result)
         model_predictions.extend(predictions["tags"])
 
     for instance, prediction in zip(dataset.instances, model_predictions):
         fields = instance.fields
         try:
-            # Most sentences have a verbal predicate, but not all.
+            # most sentences have a verbal predicate, but not all.
             verb_index = fields["verb_indicator"].labels.index(1)
         except ValueError:
             verb_index = None
 
         gold_tags = fields["tags"].labels
-        sentence = fields["tokens"].tokens
+        sentence = [x.text for x in fields["tokens"].tokens]
 
         write_to_conll_eval_file(prediction_file, gold_file,
                                  verb_index, sentence, prediction, gold_tags)
@@ -60,10 +68,11 @@ def main(serialization_directory, device):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Write CONLL format SRL predictions"
+    parser = argparse.ArgumentParser(description="write conll format srl predictions"
                                                  " to file from a pretrained model.")
-    parser.add_argument('--path', type=str, help='The serialization directory.')
-    parser.add_argument('--device', type=int, default=-1, help='The device to load the model onto.')
-
+    parser.add_argument('--path', type=str, help='the serialization directory.')
+    parser.add_argument('--device', type=int, default=-1, help='the device to load the model onto.')
+    parser.add_argument('--data', type=str, default=None, help='A directory containing a dataset to evaluate on.')
+    parser.add_argument('--prefix', type=str, default="", help='A prefix to distinguish model outputs.')
     args = parser.parse_args()
-    main(args.path, args.device)
+    main(args.path, args.device, args.data, args.prefix)
