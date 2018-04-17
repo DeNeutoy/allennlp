c93b262f8fa30cb7a12f77861bb9947675bf742b
Mark Neumann <markng@allenai.org>
2018-04-17 13:06:22 -0700

    add bio glove experiments

---
+++ beaker_configs/genia/run_bio_glove.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+source activate allennlp
+CONFIG_PATH="beaker_configs/genia"
+# Baseline, no elmo.
+
+python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline.json --source "ds_33fmprd6uv1g:/glove" --source "genia-ptb:/genia-ptb" \
+                                                            --memory 10GB --gpu_count 1 --desc "baseline, bio glove only"
+
+export ELMO_OPTIONS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+export ELMO_WEIGHTS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+export ELMO="elmo_2x4096_512_2048cnn_2xhighway_options.1"
+export NAME="regular elmo"
+
+python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo_glove.json --source "ds_33fmprd6uv1g:/glove" --source "genia-ptb:/genia-ptb" \
+                                                             --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME with bio glove" \
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS"
+
+
+export ELMO_OPTIONS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+export ELMO_WEIGHTS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_Genia_training_sentences.hdf5"
+export ELMO="ds_g8ojn0v8uot9"
+export NAME="elmo tuned on just genia training data"
+
+python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo_glove.json --source "ds_33fmprd6uv1g:/glove" --source "genia-ptb:/genia-ptb" \
+                                                             --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME with bio glove" \
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS"
+
+export ELMO_OPTIONS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+export ELMO_WEIGHTS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_weights_PubMed_small_5epochs.hdf5"
+export ELMO="ds_oavfzynyzb28"
+export NAME="elmo tuned on pubmed small data"
+
+python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo_glove.json --source "ds_33fmprd6uv1g:/glove" --source "genia-ptb:/genia-ptb" \
+                                                             --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME with bio glove" \
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS"
+
+
+export ELMO_OPTIONS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+export ELMO_WEIGHTS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_weights_PubMed_only.hdf5"
+export ELMO="ds_t3sage6lmxai"
+export NAME="elmo trained on 800 m pubmed tokens from scratch"
+
+python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo_glove.json --source "ds_33fmprd6uv1g:/glove" --source "genia-ptb:/genia-ptb" \
+                                                             --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME with bio glove" \
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS"
