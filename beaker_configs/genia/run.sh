6bf3fae9bf2a59561d6fff473046171cff26ebe0
Mark Neumann <markng@allenai.org>
2018-04-26 14:01:07 -0700

    final configs without pos tags

--- beaker_configs/genia/run.sh
+++ beaker_configs/genia/run.sh
@@ -2,15 +2,19 @@
 
 source activate allennlp
 
-SEED=$1
+export SEED=2
+export PYTORCH_SEED=5543
+export NUMPY_SEED=5765
 CONFIG_PATH="beaker_configs/genia"
 PUBMED_GLOVE="ds_33fmprd6uv1g"
 # Baseline, no elmo.
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline.json --source "glove.1:/glove" --source "genia-ptb:/genia-ptb" \
-                                                            --memory 10GB --gpu_count 1 --desc "baseline, glove only" --name "baseline-glove-only-$SEED" --env "SEED=$SEED"
-
+                                                            --memory 10GB --gpu_count 1 --desc "baseline, glove only" --name "baseline-glove-only-$SEED" \
+                                                            --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
+# baseline with pubmed glove embeddings
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_pubmed.json --source "$PUBMED_GLOVE:/glove" --source "genia-ptb:/genia-ptb" \
-                                                            --memory 10GB --gpu_count 1 --desc "baseline, glove only" --name "baseline-bio-glove-only-$SEED" --env "SEED=$SEED"
+                                                            --memory 10GB --gpu_count 1 --desc "baseline, bio glove only" --name "baseline-bio-glove-only-$SEED" \
+                                                            --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
 
 export ELMO_OPTIONS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_options.json"
 export ELMO_WEIGHTS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
@@ -20,11 +24,13 @@ export NAME="regular elmo $SEED"
 
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo.json --source "genia-ptb:/genia-ptb" \
                                                              --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME" --name "$NAME"\
-                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" --env "SEED=$SEED"
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" \
+                                                            --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
 
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo_glove.json --source "$PUBMED_GLOVE:/glove" --source "genia-ptb:/genia-ptb" \
                                                              --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME with bio glove" --name "$NAME with bio glove" \
-                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" --env "SEED=$SEED"
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" \
+                                                            --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
 
 
 export ELMO_OPTIONS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_options.json"
@@ -35,11 +41,13 @@ export NAME="elmo tuned on just genia training data $SEED"
 
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo.json --source "genia-ptb:/genia-ptb" \
                                                              --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME" --name "$NAME" \
-                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" --env "SEED=$SEED"
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" \
+                                                            --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
 
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo_glove.json --source "$PUBMED_GLOVE:/glove" --source "genia-ptb:/genia-ptb" \
                                                              --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME with bio glove" --name "$NAME with bio glove" \
-                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" --env "SEED=$SEED"
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" \
+                                                            --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
 
 export ELMO_OPTIONS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_options.json"
 export ELMO_WEIGHTS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_weights_PubMed_small_5epochs.hdf5"
@@ -48,11 +56,13 @@ export NAME="elmo tuned on pubmed small data $SEED"
 
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo.json --source "genia-ptb:/genia-ptb" \
                                                              --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME" --name "$NAME" \
-                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" --env "SEED=$SEED"
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" \
+                                                            --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
 
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo_glove.json --source "$PUBMED_GLOVE:/glove" --source "genia-ptb:/genia-ptb" \
                                                              --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME with bio glove" --name "$NAME with bio glove"\
-                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" --env "SEED=$SEED"
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" \
+                                                            --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
 
 export ELMO_OPTIONS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_options.json"
 export ELMO_WEIGHTS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_PubMed_sentences_large_5epochs.hdf5"
@@ -61,11 +71,13 @@ export NAME="elmo tuned on pubmed large data $SEED"
 
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo.json --source "genia-ptb:/genia-ptb" \
                                                              --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME" --name "$NAME"\
-                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" --env "SEED=$SEED"
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" \
+                                                            --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
 
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo_glove.json --source "$PUBMED_GLOVE:/glove" --source "genia-ptb:/genia-ptb" \
                                                              --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME with bio glove" --name "$NAME with bio glove" \
-                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" --env "SEED=$SEED"
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" \
+                                                            --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
 
 export ELMO_OPTIONS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_options.json"
 export ELMO_WEIGHTS="/elmo/elmo_2x4096_512_2048cnn_2xhighway_weights_PubMed_only.hdf5"
@@ -74,8 +86,10 @@ export NAME="elmo trained on 800 m pubmed tokens from scratch $SEED"
 
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo.json --source "genia-ptb:/genia-ptb" \
                                                              --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME" --name "$NAME"\
-                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" --env "SEED=$SEED"
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" \
+                                                             --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
 
 python scripts/ai2-internal/run_with_beaker.py $CONFIG_PATH/baseline_elmo_glove.json --source "$PUBMED_GLOVE:/glove" --source "genia-ptb:/genia-ptb" \
                                                              --source "$ELMO:/elmo" --memory 10GB --gpu_count 1 --desc "$NAME with bio glove" --name "$NAME with bio glove" \
-                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" --env "SEED=$SEED"
+                                                             --env "ELMO_OPTIONS=$ELMO_OPTIONS" --env "ELMO_WEIGHTS=$ELMO_WEIGHTS" \
+                                                             --env "SEED=$SEED" --env "PYTORCH_SEED=$PYTORCH_SEED" --env "NUMPY_SEED=$NUMPY_SEED"
