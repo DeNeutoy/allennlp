ef6ca4cddd335129173ca02902e209e63709d30d
Mark Neumann <markng@allenai.org>
2018-08-28 18:30:05 -0700

    remove prints

--- allennlp/nn/util.py
+++ allennlp/nn/util.py
@@ -439,8 +439,6 @@ def _last_dimension_applicator(function_to_apply: Callable[[torch.Tensor, Option
     if mask is not None:
         while mask.dim() < tensor.dim():
             mask = mask.unsqueeze(1)
-        print(mask.size())
-        print(tensor.size())
         mask = mask.expand_as(tensor).byte().contiguous()
         mask = mask.view(-1, mask.size()[-1])
     reshaped_result = function_to_apply(reshaped_tensor, mask)
